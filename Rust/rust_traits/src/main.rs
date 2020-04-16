59cde55636f6ecbf7eeea7855c70f0ad10c3bd5d
hjiang <hjiang@sse.com.cn>
2020-04-16 15:24:17 +0800

    commit

--- Rust/rust_traits/src/main.rs
+++ Rust/rust_traits/src/main.rs
@@ -1,7 +1,10 @@
-use std::fmt;
+use std::fmt::Display;
 
 pub trait Summary {
     fn summary(&self) -> String;
+    fn greeting(&self) -> String {
+        format!("Hola amigo")
+    }
 }
 
 pub struct NewsArticle {
@@ -11,6 +14,12 @@ pub struct NewsArticle {
     pub content: String,
 }
 
+impl Summary for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} {}", self.headline, self.author, self.location)
+    }
+}
+
 pub struct Tweet {
     pub username: String,
     pub content: String,
@@ -18,87 +27,101 @@ pub struct Tweet {
     pub retweet: bool,
 }
 
-impl Summary for NewsArticle {
-    fn summary(&self) -> String {
-        format!("{}, by {} at {}", self.headline, self.author, self.location)
-    }
-}
-
 impl Summary for Tweet {
     fn summary(&self) -> String {
-        format!("{}, by {}", self.content, self.username)
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn greeting(&self) -> String {
+        format!("Hola amigo, from {}", self.username)
     }
 }
 
-impl fmt::Display for NewsArticle {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "({}, {}, {}, {})",
-            self.headline, self.location, self.author, self.content
-        )
-    }
-}
-
-pub fn notify(item: &impl Summary) {
+pub fn notify(item: impl Summary) {
     println!("Breaking news: {}", item.summary())
 }
 
-pub fn new_notify<T: Summary>(item: &T) {
-    println!("Breaking news: {}", item.summary())
-}
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-pub fn summ_and_print<T: Summary + fmt::Display>(elem: &T) {
-    println!("summary: {}", elem.summary());
-    println!("display: {}", elem);
-}
-
-pub fn summ_and_print_where<T>(elem: &T)
-where
-    T: Summary + fmt::Display,
-{
-    println!("summary: {}", elem.summary());
-    println!("display: {}", elem);
-}
-
-pub fn return_summarizable() -> impl Summary {
-    NewsArticle {
-        headline: String::from("the deadth number caused by corona is soaring!"),
-        location: String::from("new york city"),
-        author: String::from("hjiang"),
-        content: String::from("make america great again!"),
-    }
-}
-
-pub fn return_summarizable_new(switch: bool) -> impl Summary {
-    if switch {
-        NewsArticle {
-            headline: String::from("the deadth number caused by corona is soaring!"),
-            location: String::from("new york city"),
-            author: String::from("hjiang"),
-            content: String::from("make america great again!"),
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
-    } else {
-        Tweet {
-            username: String::from("hjiang"),
-            content: String::from("this is the end of the world"),
-            reply: false,
-            retweet: false,
+    }
+
+    largest
+}
+
+fn ref_largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = &item;
+        }
+    }
+
+    largest
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest number is x = {}", self.x);
+        } else {
+            println!("The largest number is y = {}", self.y);
         }
     }
 }
 
 fn main() {
-    let news = NewsArticle {
-        headline: String::from("the deadth number caused by corona is soaring!"),
-        location: String::from("new york city"),
-        author: String::from("hjiang"),
-        content: String::from("make america great again!"),
+    let tweet = Tweet {
+        username: String::from("hjiang"),
+        content: String::from("rust traits is something like golang interface"),
+        reply: false,
+        retweet: false,
     };
 
-    notify(&news);
-    new_notify(&news);
-    summ_and_print(&news);
-    summ_and_print_where(&news);
-    notify(&return_summarizable());
+    let news = NewsArticle {
+        headline: String::from("the death of american coronvirous is soaring"),
+        location: String::from("new york city"),
+        author: String::from("hjiang"),
+        content: String::from("unknown place"),
+    };
+
+    println!("the summary of the tweet: {}", tweet.summary());
+    println!("the greeting from the tweet: {}", tweet.greeting());
+    println!("the summary of the news artical: {}", news.summary());
+    println!("the greeting from the news artical: {}", news.greeting());
+
+    notify(tweet);
+    notify(news);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let largest_num = largest(&number_list);
+    println!("largest number in list: {}", largest_num);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let largest_char = largest(&char_list);
+    println!("largest character in list: {}", largest_char);
+
+    let refla = ref_largest(&number_list);
+    println!("ref_largest number in list: {}", refla);
+
+    let pair = Pair { x: 10, y: 20 };
+    let new_pair = Pair::new(20, 30);
+    pair.cmp_display();
+    new_pair.cmp_display();
 }
--- Rust/rust_traits/target/debug/rust_traits.d
+++ Rust/rust_traits/target/debug/rust_traits.d
@@ -1,1 +1,1 @@
-/Users/hjiang/project/rust/rust_traits/target/debug/rust_traits: /Users/hjiang/project/rust/rust_traits/src/main.rs
+/home/hjiang/project/rust/rust_traits/target/debug/rust_traits: /home/hjiang/project/rust/rust_traits/src/lib.rs /home/hjiang/project/rust/rust_traits/src/main.rs
