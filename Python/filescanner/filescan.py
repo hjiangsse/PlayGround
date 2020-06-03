6465610058186cf14ce53ee75483ea71b1a8bf99
hjiang <hjiang@sse.com.cn>
2020-06-03 10:40:06 +0800

    commit

--- Python/filescanner/filescan.py
+++ Python/filescanner/filescan.py
@@ -2,31 +2,53 @@
 import sys
 import os
 import re
+import json
 
-if len(sys.argv) < 3 :
-    print("Usage: ./filescan.py path keyword...")
+# load config file set by user
+def load_configs(cfgpath) :
+    with open(cfgpath) as cf:
+        return json.load(cf)
 
+# only search key as a word
+def trans_regex(key):
+    return "%s%s%s" % ("^(.*?(\\b", key, "\\b)[^$]*)$")
+
+
+configs = load_configs("./configs.json")
+    
 # dict to storage file discriptor
 filemap = {}
 
 # create files strore the match results, store fds in a dict
-for key in sys.argv[2:] :
-    resfilename = key + ".txt"
+for key in configs["keys"] :
+    if not os.path.isdir(configs["respath"]):
+        try:
+            os.mkdir(configs["respath"])
+        except OSError:
+            sys.exit("Creation of the directory %s failed" % configs["respath"])
+        
+    resfilename = os.path.join(configs["respath"], key + ".txt")
     resfilefd = open(resfilename, "w")
     filemap[key] = resfilefd
 
 # walk recursively 
-for root, subdirs, files in os.walk(sys.argv[1]):
+for root, subdirs, files in os.walk(configs["srcpath"]):
     # travese all files
     for filename in files:
-        fname = os.path.join(root, filename)
-        #match all keywords for current file lines
+        if filename == "configs.json" or filename in configs["filtername"]:
+            continue
+        fname = os.path.join(root, filename).encode('utf-8').strip()
+        # match all keywords for current file lines
         with open(fname) as fp:
             for cnt, line in enumerate(fp):
-                for key in sys.argv[2:]:
-                    matchobj = re.search(key, line, re.I)
-                    if matchobj and key in filemap:
-                            filemap[key].write("%s:%d,%s" % (fname, cnt, line))
+                for key in configs["keys"]:
+                    if "word" in configs["scanmode"]:
+                        matchobj = re.search(trans_regex(key), line)
+                        if matchobj and key in filemap:
+                            filemap[key].write("%s,%d,%s" % (fname, cnt, line))
+                    if "contain" in configs["scanmode"]:
+                        if key in line:
+                            filemap[key].write("%s,%d,%s" % (fname, cnt, line))
                             
 # close all the match result file
 for key in filemap:
