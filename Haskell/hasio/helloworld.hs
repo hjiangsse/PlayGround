ab930a4a702200769a997cd94ce3021ce66f4e0f
hjiang <hjiang@sse.com.cn>
2020-06-14 21:09:13 +0800

    commit

--- Haskell/hasio/helloworld.hs
+++ Haskell/hasio/helloworld.hs
@@ -108,7 +108,6 @@ main = do
 main = do
   rs <- sequence [getLine, getLine, getLine]
   print rs
--}
 
 import Control.Monad
 import Data.Char
@@ -117,3 +116,41 @@ main = forever $ do
   putStr "Give me some food: "
   input <- getLine
   putStrLn $ map toUpper input
+-}
+
+{-
+import Control.Monad
+
+main = do
+  colors <- forM [1,2,3,4] (\a -> do
+       putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"
+       color <- getLine
+       return color)
+  putStrLn "The colors that you associate with 1,2,3 and 4 are: "
+  mapM putStrLn colors
+
+import Control.Monad
+import Data.Char
+
+main = forever $ do
+  input <- getContents
+  putStrLn $ map toUpper input
+
+
+main = do
+  contents <- getContents
+  putStr (shortLinesOnly contents)
+-}
+
+{-
+main = interact shortLinesOnly
+
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+    let allLines = lines input
+        shortLines = filter (\line -> length line < 10) allLines
+        result = unlines shortLines
+    in result
+-}
+
+main = interact $ unlines . filter ((<10) . length) . lines
