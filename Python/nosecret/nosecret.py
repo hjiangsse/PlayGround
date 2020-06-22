0a4607f18650df5c9f2457a9872afdd08665764e
hjiang <hjiang@sse.com.cn>
2020-06-22 09:58:41 +0800

    commit

--- Python/nosecret/nosecret.py
+++ Python/nosecret/nosecret.py
@@ -5,20 +5,38 @@ import codecs
 import re
 import struct
 
+# encrypt account
+def accountEncrypt(origAcct, addNum):
+    acctType = origAcct[0]
+    acctNum = origAcct[1:]
+    reversedNum = acctNum[::-1]
+    newNum = (int(reversedNum) + addNum) % pow(10, 9)
+    newStrNum = str(newNum)
+    return chr(acctType) + newStrNum
+
 # process bianry file, mess the accout field
-def processBinaryFile(inpath, outpath, recsize ,fieldnum, cfginfo):
-    with open(inpath, "rb") as file:
+def processBinaryFile(inpath, outpath, recsize ,fieldnum, cfginfo, addnum):
+    with open(inpath, "rb") as file, open(outpath, "wb") as output:
         bytes = file.read(int(recsize))
         while bytes:
             for i in range (1, int(fieldnum) + 1):
                 startoffset = int(cfginfo["[Field" + str(i) + "]"][0])
                 endoffset = int(cfginfo["[Field" + str(i) + "]"][1])
                 curacc = bytes[startoffset - 1:endoffset -1]
+                
                 fmtstr = "<%ds" % (endoffset - startoffset)
+                stracc = struct.unpack(fmtstr, curacc)[0]
+                encryptedAcct = accountEncrypt(stracc, addnum)
+
+                #write the encrypted account back to the stream
+                packfmtstr = "<%ds" % (endoffset - startoffset)
+                packedEncryAcct = struct.pack(packfmtstr, bytearray(encryptedAcct, 'utf-8'))
+                bytes = bytes[:startoffset-1] + packedEncryAcct + bytes[endoffset-1:]
                 
-                stracc = struct.unpack(fmtstr, curacc)
-                print(stracc[0])
-                
+            #write the new byte to the output
+            output.write(bytes)
+
+            #read the next block of record
             bytes = file.read(int(recsize))
 
 # get accouts fields information
@@ -70,6 +88,7 @@ def main(argv):
     config.read(configfile)
 
     # get total file number in config file
+    addNum = int(config['Common']['AddNum'])
     totalFileNum = int(config['FileNum']['totalfilenum'])
 
     # process each file config
@@ -84,7 +103,8 @@ def main(argv):
         curFileInfos = curFileCfgInfo['FieldNum']
         accFieldsNum, FieldsInfo = getFieldsInfo(curFileInfos)
 
-        processBinaryFile(curFileInPath, curFileOutPath, curFileRecSize, accFieldsNum, FieldsInfo)
+        if curFileType == "BIN":
+            processBinaryFile(curFileInPath, curFileOutPath, curFileRecSize, accFieldsNum, FieldsInfo, addNum)
 
 if __name__ == "__main__":
     # execute only if run as a script
